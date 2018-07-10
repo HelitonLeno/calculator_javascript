83cc89a9d85b110237752b8bff845bfb95a4722c
HelitonLeno <ciencia_@live.com>
2018-07-10 18:02:04 -0300

    add eventos e cursor pointer nos botoes

--- js/controller/CalcController.js
+++ js/controller/CalcController.js
@@ -3,10 +3,13 @@ class CalcController {
     constructor() {
         this._locale = 'pt-BR';
         this._currentDate;
+
         this._displayCalcEL = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
+
         this.initialize();
+        this.initButtonsEvents();
     }
 
     initialize() {
@@ -17,8 +20,30 @@ class CalcController {
 
     }
 
+    addEventListenerAll(element, events, fn) {
+
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        });
+
+    }
+
     initButtonsEvents() {
-        let buttons = document.querySelector("#buttons > g, #parts > g");
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index) => {
+
+            this.addEventListenerAll(btn, 'click drag mouseover', (e) => {
+
+                console.log(btn.className.baseVal.replace('btn-', ''));
+
+            });
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e=> {
+               btn.style.cursor = "pointer";
+            });
+        });
+
     }
 
     setDisplayDateTime() {
